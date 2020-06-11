0a85fb2e7751c0c5dac43dc77aaf85a934354fc2
Eduardo <60039159+RexxzBR@users.noreply.github.com>
2020-06-11 20:32:17 -0300

    updated

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,9 @@ import {errors} from 'celebrate';
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(routes);
 
 app.use(errors());
@@ -15,6 +17,6 @@ app.use(errors());
 app.use((req, res) => {
     res.status(404).json({error: "Sorry can't find that!"})
 })
+const port = process.env.PORT || 3333;
 
-
-app.listen(process.env.PORT || 3333);
+app.listen(port);
